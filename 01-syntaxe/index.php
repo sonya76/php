5fa88ab7bd0c2389f5391e837d8b3167a31fe683
valcasen <valentincasen@live.fr>
2022-05-09 10:29:11 +0200

    commentaire en php

---
+++ 01-syntaxe/index.php
@@ -0,0 +1,16 @@
+<h1>Un titre</h1>
+
+<?php
+// echo/print
+echo 'Texte en php' , ' Autre texte<br>';
+//print 'Texte en php avec print<br>' , 'Autre texte avec print';
+
+// un commentaire
+# un commentaire
+/*
+* commmentaires
+* sur plusieurs
+* lignes
+*/
+
+?>
