a2d60540c5362349c59d7b9afed5040a91dd6403
valcasen <valentincasen@live.fr>
2022-05-09 11:30:11 +0200

    declaration de variables

--- 01-syntaxe/index.php
+++ 01-syntaxe/index.php
@@ -1,8 +1,8 @@
-<h1>Un titre</h1>
+<!-- <h1>Un titre</h1> -->
 
 <?php
 // echo/print
-echo 'Texte en php' , ' Autre texte<br>';
+// echo 'Texte en php' , ' Autre texte<br>';
 //print 'Texte en php avec print<br>' , 'Autre texte avec print';
 
 // un commentaire
@@ -12,5 +12,55 @@ echo 'Texte en php' , ' Autre texte<br>';
 * sur plusieurs
 * lignes
 */
+
+// Variables
+$variable = 'Une variable';
+$Variable = 'Une autre variable';
+//echo $variable , $Variable;
+
+//$3var = 34;
+$_3var = 34;
+//echo $_3var;
+
+$camelCase = 'une variable en camel case';
+$snake_case = 'une variable en snake case';
+$PascaleCase = 'une variable pascale case';
+$var = 'une variable en kebab-case';
+
+// string
+$nom = 'mohamed';
+// int
+$nombre = 34;
+// float
+$nombreDecimale = 23.7;
+// bool
+$vrai = true;
+$faux = false;
+// array/objet
+$tableau = ['grouge',34,45.6,false];
+// null
+$variable = null;
+
+// simple/double guillemets(quotes)
+// $message = "aujourd'hui";
+// $message = 'aujourd\'hui';
+
+$txt = 'Bonjour';
+// echo '$txt tout le monde<br>';
+// echo "$txt tout le monde";
+
+// concatenation
+$fruit1 = 'Kiwi';
+$fruit1 = 'Pomme';
+$fruit2 = 'Banane';
+
+echo $fruit1 . '<br>' . $fruit2; 
+
+
+
+
+
+
+
 
 ?>
