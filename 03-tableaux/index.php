58304a7d1ae1e6b86219a0b23a6e87775d4e195d
valcasen <valentincasen@live.fr>
2022-05-12 10:35:12 +0200

    ajout readme

--- 03-tableaux/index.php
+++ 03-tableaux/index.php
@@ -13,20 +13,30 @@ $tableau = [
     [45,67,78]
 ];
 echo '<pre>';
-// print_r($tableau2);
+// print_r($tableau);
 
 $tableau[] = 'Janvier';
 $tableau[] = 45;
 $tableau[] = false;
-// var_dump($tableau);
+array_push($tableau,'voiture'); // ajout de voiture au tableau
+array_push($tableau,'voiture1', 'voiture2');
+
+echo count($tableau); // nombre elements dans un tableau
+echo '<hr>';
+
+// print_r($tableau);
+//  var_dump($tableau);
 
 // echo $tableau[0];
 
 $notes = [12,5,16,7];
 // Afficher la moyenne des notes 
-$sommeNotes = $notes[0] + $notes[1] + $notes[2] + $notes[3];
-$moyenne = $sommeNotes / 4;
+// $sommeNotes = $notes[0] + $notes[1] + $notes[2] + $notes[3];
+// $moyenne = $sommeNotes / 4;
+$moyenne = ($notes[0] + $notes[1] + $notes[2] + $notes[3]) / 4;
 // echo $moyenne;
 
+$tab3 = array_merge($tableau2,$tableau,$notes); // assemble des tableaux
+var_dump($tab3);
 
 echo '</pre>';
