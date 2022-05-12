0e82922431ce7679b626a12d33afae783cea25de
valcasen <valentincasen@live.fr>
2022-05-12 12:33:13 +0200

    fin tableaux indexé

--- 03-tableaux/index.php
+++ 03-tableaux/index.php
@@ -33,10 +33,69 @@ $notes = [12,5,16,7];
 // Afficher la moyenne des notes 
 // $sommeNotes = $notes[0] + $notes[1] + $notes[2] + $notes[3];
 // $moyenne = $sommeNotes / 4;
-$moyenne = ($notes[0] + $notes[1] + $notes[2] + $notes[3]) / 4;
+// $moyenne = ($notes[0] + $notes[1] + $notes[2] + $notes[3]) / 4;
 // echo $moyenne;
 
 $tab3 = array_merge($tableau2,$tableau,$notes); // assemble des tableaux
-var_dump($tab3);
+// var_dump($tab3);
+
+// for($i=0; $i < count($tableau2);$i++){
+    // echo $tableau2[$i] . '<br>';
+// }
+
+$fruits = ['Banane','Kiwi','Pomme','Poire'];
+
+// $html = '';
+// for($i = 0;$i < count($fruits);$i++) {
+    // $html .= $fruits[$i] . ', ';
+// }
+// echo $html;
+
+// foreach($fruits as $fruit)
+// foreach($fruits as $value)
+{
+    // echo $value . '<br>';
+    // echo $fruit . '<br>';
+}
+
+// Construire un tableau $nombres à l'aide la boucle for contenant les nombres de 34 à 78
+$nombres = [];
+for ($k = 34;$k < 78;$k++)
+{
+    // $nombres[] = $k;
+    array_push($nombres,$k);
+}
+
+$nombres[3] = 100; 
+$nombres[0] .= 'kiwi';
+
+print_r($nombres);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 echo '</pre>';
+
+
