d5ef843abff8c0d53ebefb25d830afa3189c7f2d
valcasen <valentincasen@live.fr>
2022-05-13 12:04:05 +0200

    excercice tableau multidimensionnel:will

--- 03-tableaux/index.php
+++ 03-tableaux/index.php
@@ -181,6 +181,21 @@ echo '<hr>';
 // echo $tab['planete'][0];
 // Ecrire le mot CEPPIC
 
+echo $tab[0][2].$tab[2][0].$tab[2][2].$tab[2][2].$tab[2][3].$tab[0][2];
+
+$tab2 = [
+    [0,1],
+    [
+        2,
+        [2,3]
+    ]
+    ];
+// Afficher la valeur 3
+
+
+
+
+
 
 
 
