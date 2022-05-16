4778df6a8d533cab066a4a5b20037b83c4b8aade
formk <formk@kermed.net>
2022-05-16 16:21:01 +0200

    correction exercice annee

--- 05-boucles/index.php
+++ 05-boucles/index.php
@@ -2,16 +2,43 @@
 // Loops
 
 // While
-$j = 0;
-while ($j <= 10) {
-    // echo $j . '-';
-    echo "$j-";
-    $j++; // $j = $j + 1
-}
+// $j = 0;
+// while ($j <= 10) {
+//     // echo $j . '-';
+//     echo "$j-";
+//     $j++; // $j = $j + 1
+// }
 
 // Avec une boucle while afficher toutes les années de 1970 à aujourd'hui dans une liste ul
 // Afficher le nombre de jour depuis 1970
 // Travailler dans une branche annees
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boucle While</title>
+</head>
+<body>
+    <ul>
+<?php
+    $anne1 = 1970;
+    $anne2 = 2022;
+    $nbJour = 0;
+    while($anne1 <= $anne2){
+?>
+    <li><?=$anne1;?></li>
+<?php
+    $nbJour += 365;
+    $anne1++;
+    }
+?>   
+<li>Le nombre de jour depuis 1970 est : <strong><?=$nbJour - 365;?></strong></li>
+</ul>
+</body>
+</html>
 
 
 
