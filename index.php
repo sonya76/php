2d311ac89d4920d6197d6a23142b06a9dd90ec91
formkw11virtdar <formk@kermed.net>
2022-05-18 07:07:17 -0700

    ajout balise html dans index de php

--- index.php
+++ index.php
@@ -1,15 +1,26 @@
-<?php
-// phpinfo();
-?>
-<ul>
-    <li><a href="./01-syntaxe/index.php">Syntaxe</a></li>
-    <li><a href="./02-variables/index.php">Variables</a></li>
-    <li><a href="./03-tableaux/index.php">Tableaux(array)</a>
-        <ul>
-            <li><a href="https://www.php.net/manual/fr/book.array.php">Doc php array</a></li>
-            <li><a href="./03-tableaux/php-json.php">Php JSON</a></li>
-        </ul>
-    </li>
-    <li><a href="./04-conditions/">Conditions</a></li>
-    <li><a href="./05-boucles/index.php">Boucles</a></li>
-</ul>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+
+<body>
+    <ul>
+        <li><a href="./01-syntaxe/index.php">Syntaxe</a></li>
+        <li><a href="./02-variables/index.php">Variables</a></li>
+        <li><a href="./03-tableaux/index.php">Tableaux(array)</a>
+            <ul>
+                <li><a href="https://www.php.net/manual/fr/book.array.php">Doc php array</a></li>
+                <li><a href="./03-tableaux/php-json.php">Php JSON</a></li>
+            </ul>
+        </li>
+        <li><a href="./04-conditions/">Conditions</a></li>
+        <li><a href="./05-boucles/index.php">Boucles</a></li>
+    </ul>
+</body>
+
+</html>
