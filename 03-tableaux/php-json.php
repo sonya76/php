b9e49cb2c77fddcbd28a493ebdffb7903d2e9b71
mmm <moi@moi.fr>
2022-05-19 21:27:22 +0200

    ajout true a json_decode

--- 03-tableaux/php-json.php
+++ 03-tableaux/php-json.php
@@ -1,4 +1,5 @@
 <?php
+/*
 $person1 = [
     'nom' => 'Carle',
     'prenom' => 'Awa',
@@ -18,6 +19,9 @@ $people = [
         'email' => 'ludu@gmail.com'
     ]
     ];
+
+*/
+
 echo '<pre>';
 // print_r($people);
 
@@ -25,20 +29,33 @@ echo '<pre>';
 // echo 'Le nom de Lucie est : ' . $people[2]['nom'] . PHP_EOL;
 // echo 'L\email de Lucie est : ' . $people[2]['email'];
 echo '<hr>';
-$json_file = json_encode($people);
+//$json_file = json_encode($people);
 // echo $json_file;
 
-$json_obj = '{
-    "nom" : "Durand",
-    "prenom": "Michel",
-    "email" : "dumi@gmail.com"
-}';
+//$json_obj = '{
+//    "nom" : "Durand",
+//    "prenom": "Michel",
+//    "email" : "dumi@gmail.com"
+//}';
 // print_r($json_obj);
 // echo '<hr>';
 // $json_array_php = json_decode($json_obj);
 // print_r($json_array_php);
 
-// 
+// Lorsque on ajoute la variable booléenne true à la fonction json_decode elle retourne un tableau associatif
+
+$json_obj = '{
+    "nom" : "Durand",
+    "prenom": "Michel",
+    "email" : "dumi@gmail.com",
+    "couleur" : [ "rouge" , "vert" , "jaune" ],
+    "image" : "https://ximg.es/64x64/000/fff"
+}';
+//$json_array_php = json_decode($json_obj,true);
+
+//print_r($json_obj);
+
+//echo $json_array_php['couleur'][2];
 
 
 echo '</pre>';
